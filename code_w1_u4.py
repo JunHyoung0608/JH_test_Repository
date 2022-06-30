39527ec04ce09be2f8118b53a6305c3234dcf3be
JunHyoung0608 <wnsgud1235686@gmail.com>
2022-06-30 14:14:47 +0900

    PYTHON WEEK1 UNIT4

---
+++ code_w1_u4.py
@@ -0,0 +1,30 @@
+#세미콜론
+print('Hello, world!')
+print('Hello'); print('1234')
+print('--------------------')
+
+#한줄주석
+print('Hello, world!')
+#print('Hello, world!')
+a=1+2 #더하기
+print('Hello, world!') #printf('1234567890')
+print('--------------------')
+
+#블록주석
+#print('Hello, world!')
+#print('1234567890')
+print('--------------------')
+
+#드려쓰기
+a=10
+if a==10:
+    print('10입니다.') #4칸 드려쓰기
+if a==10:
+  print('10입니다.') #2칸 드려쓰기
+print('--------------------')
+
+#코드 블록
+a=10
+if a==10:      
+    print('10')
+    print('입니다') #2줄 4칸 드려쓰기
