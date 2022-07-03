546cb91e5b47dd65c63f83cf17e8db7600871da1
JunHyoung0608 <wnsgud1235686@gmail.com>
2022-07-03 13:40:24 +0900

    Python Week1 Unit7

---
+++ code_w1_u7.py
@@ -0,0 +1,40 @@
+#값을 여러개 출력하기
+print(1,2,3)
+print('Hello','Python')
+print('--------------------')
+
+#sep로 값 사이에 문자 넣기
+print(1,2,3, sep=', ')
+print(4,5,6, sep=',')
+print('Hello','Python', sep='')
+print(1920,1080, sep='X')
+print('--------------------')
+
+#줄바꿈 활용하기
+print(1,2,3, sep='\n')
+print('1\t2\t3\t') #제어문자\n,\t,... -> 이스케이프 시퀀스:\뒤에 문자나 숫자의 조합을 부른다.
+print('--------------------')
+
+#end 사용하기
+print(1, end='')
+print(2, end='')
+print(3)
+print('--------------------')
+
+#연습문제
+year=2000
+month=10
+day=27
+hour=11
+minute=43
+second=59
+
+print(year,month,day, sep='/', end=' ')
+print(hour,minute,second, sep=':')
+print('--------------------')
+
+#심사문제
+year,month,day,hour,minute,second=map(int,input().split())
+print(year,month,day, sep='-', end='')
+print('T', end='')
+print(hour,minute,second, sep=':')
